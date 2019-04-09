6ceaa97c20d372331cd06193fb6f916c46b979a4
Shrikant Sharma <shrikantsharma359@gmail.com>
2019-04-09 18:57:30 +0530

    hello

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-
+const fs=require ('fs');
 var expr=require('express');
 var hbs=require('hbs');
 
 var app=expr();
-
+const port = process.env.PORT || 3000;
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('View String','hbs');
 app.use(expr.static(__dirname+'/public'));
@@ -55,6 +55,10 @@ MultiplatformwithMobileApp:'Multiplatform with Mobile App:A'
      });
 });
 
-app.listen(3000,()=>{
+/*app.listen(3000,()=>{
 	console.log('server is upto on port 3000!');
+});*/
+
+app.listen (port,()=>{
+	console.log('server is upto on port,{port}');
 });
